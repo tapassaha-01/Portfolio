39468ea0ca1fabe9299b902e1a4df62d6d39a535
tsaha1 <tapassaha6685@gmail.com>
2024-10-27 16:53:23 +0530

    completed form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee ,faPhone} from '@fortawesome/free-solid-svg-icons';
+import { faUser,faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule,
+    ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
 
+  faCoffee = faCoffee;
+  faUser = faUser;
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  registrationForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.registrationForm = this.formBuilder.group({
+      fullname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+      console.log('Form Submitted!', this.registrationForm.value);
+   
+  }
 }
